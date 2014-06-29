82f54f6581a4eccb6b833b088432e9be43f064b6
Loide <loide.marwanga@yale.edu>
2014-06-29 13:54:24 -0400

    Finish sign-in-out

--- cwc/app/controllers/users_controller.rb
+++ cwc/app/controllers/users_controller.rb
@@ -8,12 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the Sample App!"
-      	redirect_to @user
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      	render 'new'
+      render 'new'
     end
   end
 
--- cwc/spec/models/user_spec.rb
+++ cwc/spec/models/user_spec.rb
@@ -24,6 +24,7 @@ describe User do
  it { should respond_to(:password) }
  it { should respond_to(:password_confirmation) }
  it { should respond_to(:authenticate) }
+ it { should respond_to(:remember_token) }
 
  it { should be_valid }
 
@@ -136,4 +137,9 @@ describe "return value of authenticate method" do
       specify { expect(user_for_invalid_password).to be_false }
     end
 end
+
+describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
