75f5490072873934ea68a29f508bfc8390059875
Loide <loide.marwanga@yale.edu>
2014-06-29 11:44:41 -0400

    Added sign up page

--- cwc/config/routes.rb
+++ cwc/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root  'static_pages#home'
-
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
