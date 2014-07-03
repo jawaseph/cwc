eb0a81aa9306ca3fdf66db9c5cbe83269aee0bf3
Loide <loide.marwanga@yale.edu>
2014-07-03 16:48:51 -0400

    Worked on profile page

--- cwc/app/assets/javascripts/application.js
+++ cwc/app/assets/javascripts/application.js
@@ -1,16 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+YUI({
+    classNamePrefix: 'pure'
+}).use('gallery-sm-menu', function (Y) {
+
+    var horizontalMenu = new Y.Menu({
+        container         : '#topmenu',
+        sourceNode        : '#menu_items',
+        orientation       : 'horizontal',
+        hideOnOutsideClick: false,
+        hideOnClick       : false
+    });
+
+    horizontalMenu.render();
+    horizontalMenu.show();
+
+});
--- cwc/app/controllers/users_controller.rb
+++ cwc/app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @results = Craigslist.newhaven.missed_connections.fetch(5)
+    @results
   end
 
   def create
